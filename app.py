7d15b82b825686f699f51149d70c9fa74cba623d
Murilo Gomes de Oliveira <159261576+MuriloGomesOli@users.noreply.github.com>
2024-11-27 16:35:52 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from modelos.carro import Carro
+from modelos.interface import Interface as i
+    
+
+def main():
+        i.apresentar_menu_principal()
+    
+
+if __name__ == '__main__':
+        
+        main()
